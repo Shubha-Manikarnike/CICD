43ed698516117eed8f003b23fd20c7701628f8fa
Shubha Manikarnike <shubha.vasudev@gmail.com>
2023-08-14 11:22:32 +0530

    Update test_sample_1.py
    Intentionally fail testcases

--- test_sample_1.py
+++ test_sample_1.py
@@ -2,8 +2,8 @@ import pytest
 def test_file1_method1():
 	x=5
 	y=6
-	assert x+1 == y,"test failed"
-	assert x == y-1,"test failed"
+	assert x == y,"test failed"
+	assert x == y,"test failed"
 def test_file1_method2():
 	x=5
 	y=6
